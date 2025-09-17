d813a50ea8b6269296b4a02df4f6ccabaa07767e
AtNaveen <snaveen09102002@gmail.com>
2025-09-17 22:23:00 +0530

    "Create admin panel page in react"

---
+++ admin/src/App.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react"
+import Navbar from "./compoenets/Navbar"
+import Sidebar from "./compoenets/Sidebar"
+import { Route , Routes } from "react-router-dom"
+import Add from "./pages/Add"
+import List from './pages/List'
+import Orders from './pages/Orders'
+import Login from "./compoenets/Login"
+
+const App = () => {
+
+  const [token , setToken] = useState('');
+
+  return (
+    <div className="bg-gray-50 min-h-screen">
+
+      { token === "" 
+      ? <Login />
+      : <>
+      <Navbar />
+      <hr />
+      <div className="flex w-full">
+        <Sidebar />
+        <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+          <Routes>
+            <Route path='/add' element={<Add />} />
+            <Route path='/list' element={<List />} />
+            <Route path='/orders' element={<Orders />} />
+          </Routes>
+        </div>
+      </div>
+      </>
+}
+    </div>
+  )
+}
+
+export default App
